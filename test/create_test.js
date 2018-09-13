64f1029186906c485000c58a323eaefd275c7ddc
Steven Del Rosario <stevendelro@pm.me>
2018-09-12 19:26:36 -0700

    Added assert function to check if Joe is old data.

--- test/create_test.js
+++ test/create_test.js
@@ -3,8 +3,11 @@ const User = require('../src/user');
 
 describe('Creating records', () => {
   it('saves a user', () => {
-    const joe = new User({ name: 'Joe'});
+    const joe = new User({ name: 'Joe' });
 
-    joe.save();
+    joe.save().then(() => {
+      assert(!joe.isNew);
+      done();
+    });
   });
 });
