2daab98630775d857d246658a13d71507fe47181
Steven Del Rosario <stevendelro@pm.me>
2018-09-12 19:27:40 -0700

    Placed all connect logic within a before function to account for async.

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,15 +1,22 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test', { useNewUrlParser: true });
-mongoose.connection
-  .once('open', () => console.log('Good to go!'))
-  .on('error', error => {
-    console.warn('Warning: ', error);
-  });
-
-  // This will wipe the database beforeEach test.
-  beforeEach( done => {
-    mongoose.connection.collections.users.drop(() => {
+before(done => {
+  mongoose.connect(
+    'mongodb://localhost/users_test',
+    { useNewUrlParser: true }
+  );
+  mongoose.connection
+    .once('open', () => {
       done();
+    })
+    .on('error', error => {
+      console.warn('Warning: ', error);
     });
+});
+
+// This will wipe the database beforeEach test.
+beforeEach(done => {
+  mongoose.connection.collections.users.drop(() => {
+    done();
   });
+});
