4f080d714b17a7534c48df41ab6577bfb6f1e4a3
Steven Del Rosario <stevendelro@pm.me>
2018-09-12 18:20:21 -0700

    Current URL string parser is deprecated, opting-in to the new url parser

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test');
+mongoose.connect('mongodb://localhost/users_test', { useNewUrlParser: true });
 mongoose.connection
   .once('open', () => console.log('Good to go!'))
   .on('error', error => {
