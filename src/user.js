69600cf7f3fe50bda5fe8cb13545f5ea05207255
Steven Del Rosario <stevendelro@pm.me>
2018-09-12 18:09:16 -0700

    Defined User Schema and exported it as a mongoose model

---
+++ src/user.js
@@ -0,0 +1,10 @@
+const mongood = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: String
+});
+
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
