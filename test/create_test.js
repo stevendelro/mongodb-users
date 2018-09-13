d69c21278704738743e526fb37462f53b6914214
Steven Del Rosario <stevendelro@pm.me>
2018-09-13 13:37:11 -0700

    Adding comments to reference the intention for each function block.

--- test/create_test.js
+++ test/create_test.js
@@ -4,7 +4,6 @@ const User = require('../src/user');
 describe('Creating records', () => {
   it('saves a user', done => {
     const joe = new User({ name: 'Joe' });
-
     joe.save().then(() => {
       assert(!joe.isNew);
       done();
--- test/reading_test.js
+++ test/reading_test.js
@@ -3,13 +3,14 @@ const User = require('../src/user');
 
 describe('Reading users out of the database', () => {
   let joe;
-
+  // Creating an instance of User with the name Joe.
   beforeEach(done => {
     joe = new User({ name: 'Joe' });
     joe.save().then(() => done());
   });
 
   it('finds all users with a name of Joe', done => {
+    // This was an example how the ObjectId wrapper may cause some confusion when comparing by IDs.
     User.find({ name: 'Joe' }).then(users => {
       assert(users[0].id.toString() === joe._id.toString());
       done();
@@ -17,6 +18,7 @@ describe('Reading users out of the database', () => {
   });
 
   it('find a user with a particular id', done => {
+    // This was a basic example of using the .findOne method.
     User.findOne({ _id: joe._id })
       .then( user => {
         assert(user.name === 'Joe');
