6199057f06a7fa2f3319358cd89f4542d4b6b7d3
Steven Del Rosario <stevendelro@pm.me>
2018-09-13 17:12:22 -0700

    Add postCount to the UserSchema and experimented with $inc operator.

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
   let joe;
 
   beforeEach(done => {
-    joe = new User({ name: 'Joe' });
+    joe = new User({ name: 'Joe', postCount: 0 });
     joe.save().then(() => done());
   });
 
@@ -52,12 +52,22 @@ describe('Updating records', () => {
   it('will update a modal class', done => {
     assertName(User.update({ name: 'Joe' }, { name: 'Alex' }), done);
   });
-  
+
   it('will findOneAndUpdate a modal class', done => {
     assertName(User.findOneAndUpdate({ name: 'Joe' }, { name: 'Alex' }), done);
   });
 
   it('will findByIdAndUpdate a modal class', done => {
     assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex' }), done);
+  });
+
+  // Here, we are experimenting with the $inc Update Operator.
+  it('will increment a users postCount by 1', done => {
+    User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then(user => {
+        assert(user.postCount === 1);
+        done();
+      });
   });
 });
