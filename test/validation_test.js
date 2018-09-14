0ca7000998fdc578e1541b98a0655acebe573c7a
Steven Del Rosario <stevendelro@pm.me>
2018-09-13 18:11:34 -0700

    Created validation requirements for user name length.

--- test/validation_test.js
+++ test/validation_test.js
@@ -7,6 +7,23 @@ describe('Validating records', () => {
     const validationResult = user.validateSync();
     const { message } = validationResult.errors.name;
 
-    assert(message === 'Name is required.')
+    assert(message === 'Name is required.');
+  });
+
+  it("requires a user's name longer than 2 characters", () => {
+    const user = new User({ name: 'Al' });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+
+    assert(message === 'Name must be longer than two characters.');
+  });
+
+  it('prevents invalid records from being saved', done => {
+    const user = new User({ name: 'Al' });
+    user.save().catch(validationResult => {
+      const { message } = validationResult.errors.name;
+      assert(message === 'Name must be longer than two characters.');
+      done();
+    });
   });
 });
