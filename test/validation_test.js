54d1f41693c23da1a4db5dac6fc612b3f88e04b3
Steven Del Rosario <stevendelro@pm.me>
2018-09-13 17:32:48 -0700

    Setting up validation to require a specific property on our model.

---
+++ test/validation_test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Validating records', () => {
+  it('requires a user name', () => {
+    const user = new User({ name: undefined });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+
+    assert(message === 'Name is required.')
+  });
+});
