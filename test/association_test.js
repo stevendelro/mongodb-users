d2525e0894fc6e1ddeda69474f4f1e0724574728
Steven Del Rosario <stevendelro@pm.me>
2018-09-15 15:19:56 -0700

    We just went to town with this .populate() method. Everything works.

--- test/association_test.js
+++ test/association_test.js
@@ -53,10 +53,34 @@ describe('Associations', () => {
 
   it('saves a relation between a user and a blogPost', done => {
     User.findOne({ name: 'Joe' })
-    .populate('blogPosts')
-    .then( user => {
-      assert(user.blogPosts[0].title === 'JS is Great')
-      done();
-    })
+      .populate('blogPosts')
+      .then(user => {
+        assert(user.blogPosts[0].title === 'JS is Great');
+        done();
+      });
+  });
+
+  it('saves a full relation graph', done => {
+    User.findOne({ name: 'Joe' })
+      .populate({
+        path: 'blogPosts',
+        populate: {
+          path: 'comments',
+          model: 'comment',
+          populate: {
+            path: 'user',
+            model: 'user'
+          }
+        }
+      })
+      .then(user => {
+        assert(user.name === 'Joe');
+        assert(user.blogPosts[0].title === 'JS is Great');
+        assert(
+          user.blogPosts[0].comments[0].content === 'Congrats on your post!'
+        );
+        assert(user.blogPosts[0].comments[0].user.name === 'Joe');
+        done();
+      });
   });
 });
