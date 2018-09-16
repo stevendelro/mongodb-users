62e97f3ed14f5eb9bd28114e69507dfb4c84da16
Steven Del Rosario <stevendelro@pm.me>
2018-09-15 18:44:16 -0700

    Set middlware up to delete any dangling blogposts on remove.

---
+++ test/middleware_test.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const assert = require('assert');
+const User = require('../src/user');
+const BlogPost = require('../src/blogPost');
+
+describe('Middleware', () => {
+  let joe, blogPost;
+
+  beforeEach(done => {
+    joe = new User({ name: 'Joe' });
+    blogPost = new BlogPost({
+      title: 'JS is Great',
+      content: 'Yup, it really is.'
+    });
+    joe.blogPosts.push(blogPost);
+
+    Promise.all([joe.save(), blogPost.save()]).then(() => done());
+  });
+
+  it('users clean up dangling blogposts on remove', done => {
+    joe
+      .remove()
+      .then(() => BlogPost.count())
+      .then(count => {
+        assert(count === 0);
+        done();
+      });
+  });
+});
